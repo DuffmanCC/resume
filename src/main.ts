0b3f84a86f414fa77a8569abf16dbef2ceeb9f2e
Carlos Ortiz <carlosor@gmail.com>
2022-05-13 11:17:41 +0200

    add ga manually

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"
 import './index.css'
-import VueGtag from "vue-gtag"
 
 createApp(App)
-  .use(VueGtag, {config: { id: "G-4DN7JDS8JH" }})
   .use(router)
   .mount('#app')
